c4927d296d48aaf3ff1b988dd52f10bb97e6dff5
Tania G Jimenez <122074803+TaniaGJimenez@users.noreply.github.com>
2023-09-21 21:43:33 -0600

    Añade ejercicios #Js-0 y #Js-1

---
+++ Week01.js
@@ -0,0 +1,11 @@
+// JS 0: Nombrando Variables en Js.
+// Crea variables siguiendo las reglas de nomenclatura adecuadas.
+
+// Crea 10 variables de tipo string (cadena de texto) y asignale cualquier texto. 
+let variableString = "Hola, mundo!";
+
+// Crea 10 variables de tipo number (numero) y asígnale cualquier número.
+let variableNumber = 100;
+
+// Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.
+let variableBoolean = true;
---
+++ Week02.js
@@ -0,0 +1,71 @@
+// 🚀 **JS 1: Creando Funciones en Js**
+
+// Objetivo: Aprender a crear funciones en JavaScript. 📜
+
+// Consejos clave:
+// ✅ Completá la función.
+// ✅ Todo tu código tiene que ir adentro de las llaves de la función.
+// ✅ Asegurate de usar el "return".
+// ❗ ACLARACIÓN: "console.log()" no va a funcionar.
+// ❌ No cambies los nombres de las funciones.
+
+// 1. Retornar un string.
+// function devolverString(str){
+//     // 🎯 'Return' la string provista: str
+//     // Tu código: 
+// }
+
+// 2. Retornar una suma.
+// function suma(x, y){
+//     // "x" e "y" son números: 
+//     // 🎯 suma "x" e "y" y retorná el valor
+//     // Tu código: 
+// }
+
+// 3. Retornar una resta.
+// function resta(x, y){
+//     // 🎯 resta "y" e "x" y retorná el valor
+//     // Tu código: 
+// }
+
+// 4. Retornar una multiplicación.
+// function multiplicar(x, y){
+//     // 🎯 multiplica "x" e "y" y retorná el valor
+//     // Tu código: 
+// }
+
+// 5. Retornar una división.
+// function dividir(x, y){
+//     // 🎯 dividí "x" e "y" y retorná el valor
+//     // Tu código: 
+// }
+
+// 6. Comparar números.
+// function igualdad(x, y){
+//     // 🎯 Devolvé "true" si "x" e "y" son iguales, de lo contrario devolvé "false".
+//     // Tu código: 
+// }
+
+// 7. Comparar la longitud de dos strings.
+// function compararLongitur(str1, str2){
+//     // 🎯 Devolvé "true" si las dos strings tienen la misma longitud, de lo contrario devolvé "false".
+//     // Tu código: 
+// }
+
+// 8. Comparar un parámetro I.
+// function menorQueNoventa(num){
+//     // 🎯 Devolvé "true" si el parametro de la funcion "num" es menor que noventa, de lo contrario devolvé "false".
+//     // Tu código: 
+// }
+
+// 9. Comparar un parámetro II.
+// function mayorQueCincuenta(num){
+//     // 🎯 Devolvé "true" si el parametro de la funcion "num" es mayor que cincuenta, de lo contrario devolvé "false".
+//     // Tu código: 
+// }
+
+// 10. Retornar el módulo o resto.
+// function obtenerResto(x, y){
+//     // 🎯 Obtener el resto de la división de "x" e "y".
+//     // Tu código: 
+// }
