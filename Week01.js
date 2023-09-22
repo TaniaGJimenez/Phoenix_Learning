595b5cd2997863366694f00d00a00434e638c8aa
Tania G Jimenez <122074803+TaniaGJimenez@users.noreply.github.com>
2023-09-22 08:50:03 -0600

    Week01 Completed:Assigned Names to Variables

--- Week01.js
+++ Week01.js
@@ -2,10 +2,53 @@
 // Crea variables siguiendo las reglas de nomenclatura adecuadas.
 
 // Crea 10 variables de tipo string (cadena de texto) y asignale cualquier texto. 
-let variableString = "Hola, mundo!";
+let variableString = "Hola, mundo!"; //EJEMPLO
+
+let something = "Algo";
+let RedBird = "Pajaro Rojo";
+let doingExcercises = "Haciendo Ejercicios";
+
+let cat = "Gato";
+let dog = "Perro";
+let japanCity = "Tokyo";
+
+let sciFiBook = "Ender's Game";
+let specialization = "Especialización";
+let streamingService = "Netflix";
+
+let justAnotherVariable = "Hellouuu"
+
 
 // Crea 10 variables de tipo number (numero) y asígnale cualquier número.
-let variableNumber = 100;
+let variableNumber = 100; //EJEMPLO
+
+let hitchhikerNumber = 42;
+let piValue = 3.1416;
+let someOtherNumber = 11;
+
+let largeNumber = 3092333923;
+let goldenRatio = 1.61803398875;
+let debtAmount = 30;
+
+let favoriteNumber = 7;
+let OrwellsNovel = 1984;
+let twelve = 12;
+
+let aMillion = 1000000;
 
 // Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.
-let variableBoolean = true;
+let variableBoolean = true; //EJEMPLO
+
+let isPinapplePizzaTasty = false;
+let isPenguinFlightExpert = false;
+let ismyCatSpoiled = true;
+
+let isRainbowMadeOfCheese = true;
+let isCatDogReal = false;
+let canRobotsFallInLove = false;
+
+let isCoffeeFuel = true;
+let amIaMillionaire = false;
+let isUnicornReal = true;
+
+let finishingHomework = true;
